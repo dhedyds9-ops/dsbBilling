1fc144f58903f050e6b147b5d7c226d0543db01a
winets.id <dhedyds9@gmail.com>
2026-09-22 01:18:57 +0700

    feat(acs): sync optical power to DB and add colored badges in device index

--- app/Livewire/ACS/Device/Index.php
+++ app/Livewire/ACS/Device/Index.php
@@ -52,7 +52,7 @@ class Index extends BaseACSComponent
         try {
             $acsService = new \App\Services\Adapters\Monitoring\GenieACSDriver();
             $query = [
-                'projection' => '_id,_deviceId,_lastInform,VirtualParameters,InternetGatewayDevice.DeviceInfo,Device.DeviceInfo,InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection,Device.WANDevice.1.WANConnectionDevice.1.WANPPPConnection,InternetGatewayDevice.ManagementServer.ConnectionRequestURL,Device.ManagementServer.ConnectionRequestURL'
+                'projection' => '_id,_deviceId,_lastInform,VirtualParameters,InternetGatewayDevice.DeviceInfo,Device.DeviceInfo,InternetGatewayDevice.WANDevice.1.WANConnectionDevice.1.WANPPPConnection,Device.WANDevice.1.WANConnectionDevice.1.WANPPPConnection,InternetGatewayDevice.ManagementServer.ConnectionRequestURL,Device.ManagementServer.ConnectionRequestURL,InternetGatewayDevice.WANDevice.1.X_FH_GponInterfaceConfig.RXPower,InternetGatewayDevice.WANDevice.1.X_ZTE-COM_WANPONInterfaceConfig.RXPower,InternetGatewayDevice.WANDevice.1.X_HW_PONInterfaceConfig.RXPower,InternetGatewayDevice.WANDevice.1.WANPONInterfaceConfig.1.X_ZTE-COM_RxPower,InternetGatewayDevice.WANDevice.1.WANPONInterfaceConfig.1.X_HW_RxPower,InternetGatewayDevice.WANDevice.1.WANEponInterfaceConfig.1.RxPower,Device.Optical.1.Transceiver.RxPower'
             ];
             $devices = $acsService->listDevices($query, 5000);
             $count = 0;
@@ -98,7 +98,28 @@ class Index extends BaseACSComponent
                                  $deviceData['InternetGatewayDevice']['WANDevice'][1]['WANConnectionDevice'][1]['WANPPPConnection'][1]['Username']['_value'] ?? 
                                  $deviceData['Device']['WANDevice'][1]['WANConnectionDevice'][1]['WANPPPConnection'][1]['Username']['_value'] ?? null;
 
+                $rxPower = $deviceData['InternetGatewayDevice']['WANDevice'][1]['X_FH_GponInterfaceConfig']['RXPower']['_value']
+                        ?? $deviceData['InternetGatewayDevice']['WANDevice'][1]['X_ZTE-COM_WANPONInterfaceConfig']['RXPower']['_value']
+                        ?? $deviceData['InternetGatewayDevice']['WANDevice'][1]['X_HW_PONInterfaceConfig']['RXPower']['_value']
+                        ?? $deviceData['VirtualParameters']['RXPower']['_value']
+                        ?? $deviceData['InternetGatewayDevice']['WANDevice'][1]['WANPONInterfaceConfig'][1]['X_ZTE-COM_RxPower']['_value'] 
+                        ?? $deviceData['InternetGatewayDevice']['WANDevice'][1]['WANPONInterfaceConfig'][1]['X_HW_RxPower']['_value'] 
+                        ?? $deviceData['InternetGatewayDevice']['WANDevice'][1]['WANEponInterfaceConfig'][1]['RxPower']['_value']
+                        ?? $deviceData['Device']['Optical'][1]['Transceiver']['RxPower']['_value']
+                        ?? null;
+                
+                if ($rxPower !== null && is_numeric($rxPower)) {
+                    $rxPower = (float)$rxPower;
+                    if ($rxPower < -500 || $rxPower > 500) {
+                        $rxPower /= 1000;
+                    } elseif ($rxPower < -50 || $rxPower > 50) {
+                        $rxPower /= 100;
+                    }
+                    $rxPower = round($rxPower); // Since migration is integer, we round it
+                }
+
                 $acsDeviceParams = [
+                    'signal' => $rxPower,
                     'serial_number' => $serialNumber,
                     'mac_address' => $mac,
                     'model' => $model,
