a4d0ff82577f574ab0a55f964546aa0a4c995b0d
winets.id <dhedyds9@gmail.com>
2026-09-21 20:34:40 +0700

    fix(acs): correct fiberhome wifi TR-098 parameter paths

--- config/onu-vendors.php
+++ config/onu-vendors.php
@@ -50,10 +50,10 @@ return [
         'fiberhome' => [
             'name' => 'Fiberhome',
             'wifi_parameters' => [
-                'ssid' => 'Device.WiFi.SSID.1.SSID',
-                'security_mode' => 'Device.WiFi.AccessPoint.1.Security.ModeEnabled',
-                'wpa_pre_shared_key' => 'Device.WiFi.AccessPoint.1.Security.PreSharedKey.1.PreSharedKey',
-                'wpa_passphrase' => 'Device.WiFi.AccessPoint.1.Security.KeyPassphrase',
+                'ssid' => 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.SSID',
+                'security_mode' => 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.BeaconType',
+                'wpa_pre_shared_key' => 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.PreSharedKey.1.PreSharedKey',
+                'wpa_passphrase' => 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.PreSharedKey.1.KeyPassphrase',
             ],
         ],
         'dlink' => [
